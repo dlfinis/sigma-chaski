aef7207a9dc22cef163342232db472b97ffe8963
Diego Leon <dleon@kruger.com.ec>
2015-08-27 14:48:29 -0500

    Add authentication

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,6 +6,20 @@
  */
 
 module.exports = {
-	
+	create: function (req, res) {
+    if (req.body.password !== req.body.confirmPassword) {
+      return res.json(401, {err: 'Password doesn\'t match, What a shame!'});
+    }
+    User.create(req.body).exec(function (err, user) {
+      if (err) {
+        return res.json(err.status, {err: err});
+      }
+      // If user created successfuly we return user and token as response
+      if (user) {
+        // NOTE: payload is { id: user.id}
+        res.json(200, {user: user, token: chaskiToken.issue({id: user.id})});
+      }
+    });
+  }	
 };
 
